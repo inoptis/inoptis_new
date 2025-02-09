77a06cd17818ed8d2b305c937ac2b3bdc7741b03
Alexandr Bondarenko <69047343+AlexUner@users.noreply.github.com>
2025-02-09 05:44:51 +0300

    Добавить адаптивную карту с использованием хуков useState и useEffect в ContactBlock

--- src/Components/Blocks/ContactBlock/ContactBlock.jsx
+++ src/Components/Blocks/ContactBlock/ContactBlock.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import cl from './ContactBlock.module.css'
 import icon1 from '../../../Assets/Pictures/ContactBlock/icon1.svg'
 import icon2 from '../../../Assets/Pictures/ContactBlock/icon2.svg'
 import icon3 from '../../../Assets/Pictures/ContactBlock/icon3.svg'
-// import icon4 from '../../../Assets/Pictures/ContactBlock/icon4.svg'
 import arrow from '../../../Assets/Pictures/arrow-right-contact.svg'
 import bg from '../../../Assets/Pictures/Epattern_left.svg'
-import map from '../../../Assets/Pictures/map_test.webp'
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useWindowSize } from "../../../Hooks/useWindowSize";
+
+
 const ContactBlock = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [width] = useWindowSize();
+    const [mapData, setMapData] = useState(null);
+
+    useEffect(() => {
+        const data = {
+            default: { // 1440 и больше
+                src: "https://yandex.ru/map-widget/v1/?um=constructor%3Aeaafbe59055abd83788bdb42a08855ea593553afbd6ff95aa1c294941456292f&amp;source=constructor",
+                width: 650,
+                height: 372
+            },
+            '1400-961': { // 1400-961
+                src: "https://yandex.ru/map-widget/v1/?um=constructor%3A9ed56d85e21926d84494656d48c5c97ccdbfbf8695ec34f3ac23789bd4a8098e&amp;source=constructor",
+                width: 486,
+                height: 278
+            },
+            '960-561': { // 960-561
+                src: "https://yandex.ru/map-widget/v1/?um=constructor%3Aae2261098288ccda7e7da42a6ce2a3f9066e2f8496e036f08298761cea6f5418&amp;source=constructor",
+                width: 226,
+                height: 198
+            },
+            '560': { // 560 и меньше
+                src: "https://yandex.ru/map-widget/v1/?um=constructor%3A804791f2682d5a6a05301398fe2a851e5fd5111924bc56001bc58afeeee0f589&amp;source=constructor",
+                width: "100%",
+                height: 278
+            }
+        };
+
+        let currentMapData = data.default;
+        if (width <= 1400 && width > 960)
+            currentMapData = data['1400-961'];
+        else if (width <= 960 && width > 560)
+            currentMapData = data['960-561'];
+        else if (width <= 560)
+            currentMapData = data['560'];
+
+        setMapData(currentMapData);
+    }, [width]);
+
     return (
         <div className={cl.block}>
             <img src={bg} className={cl.background} alt={'bg'}/>
@@ -22,9 +61,17 @@ const ContactBlock = () => {
                     {/*<div className={cl.item}><img src={icon4} alt={'point'}/> Москва, Высоковольтный проезд, д. 1, стр. 49, офис 326 </div>*/}
                     <div className={cl.item} onClick={() => navigate('/contacts')}>Перейти к реквизитам <img src={arrow} alt={'arrow'}/></div>
                 </div>
-                <div className={cl.mapContainer}>
-                    <img src={map} alt={'map'}/>
-                </div>
+                {mapData && (
+                    <div className={cl.mapContainer}>
+                        <iframe
+                            title="Карта местоположения"
+                            src={mapData.src}
+                            width={mapData.width}
+                            height={mapData.height}
+                            frameborder="0"
+                        ></iframe>
+                    </div>
+                )}
             </div>
         </div>
     );
