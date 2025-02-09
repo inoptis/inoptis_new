4eee3e7b99c05ad49e79abc0c106dc8a985a61fe
Alexandr Bondarenko <69047343+AlexUner@users.noreply.github.com>
2025-02-09 04:05:47 +0300

    Заменить изображение карты на скрипт Яндекс.Карт в ContactBlockV2

--- src/Components/Blocks/ContactBlock/ContactBlockV2.jsx
+++ src/Components/Blocks/ContactBlock/ContactBlockV2.jsx
@@ -21,7 +21,8 @@ const ContactBlockV2 = () => {
                     {/*</div>*/}
                 </div>
                 <div className={cl.mapContainerv2}>
-                    <img src={map} alt="map"/>
+                    {/* <img src={map} alt="map"/> */}
+                    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aeaafbe59055abd83788bdb42a08855ea593553afbd6ff95aa1c294941456292f&amp;width=600&amp;height=322&amp;lang=ru_RU&amp;scroll=true"></script>
                 </div>
             </div>
         </div>
