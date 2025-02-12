e0bc29565ade49001529a08abec2604c6926f4b1
Alexandr Bondarenko <69047343+AlexUner@users.noreply.github.com>
2025-02-12 06:09:32 +0300

    Добавить компонент SEOHelmet для управления метаданными страницы и обновить зависимости #ignore

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './Styles/Global.css'
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import SEOHelmet from './Components/SEO/SEOHelmet';
 import MyRoutes from "./Routes/Routes";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
-        <BrowserRouter>
-            <Header/>
-            <MyRoutes/>
-            <Footer/>
-        </BrowserRouter>
+      <SEOHelmet
+        title="ООО Иноптис — Сайт компании Иноптис"
+        description="НТЦ «ИНОПТИС» — детище команды профессионалов, любящих решать сложные задачи."
+        keywords="ИНОПТИС, оптика, лазерная техника, инновации"
+        url="https://inoptis.ru"
+        image="https://inoptis.ru/logo512.png"
+      />
+      <BrowserRouter>
+          <Header/>
+          <MyRoutes/>
+          <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
