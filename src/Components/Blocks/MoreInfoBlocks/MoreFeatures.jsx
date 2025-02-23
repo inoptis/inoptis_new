e00b00ff415d772927e6875d68f2161a11bf5f0b
IvanGudyushkin <144389067+IvanGudyushkin@users.noreply.github.com>
2025-02-23 17:51:41 +0300

    Update MoreFeatures.jsx

--- src/Components/Blocks/MoreInfoBlocks/MoreFeatures.jsx
+++ src/Components/Blocks/MoreInfoBlocks/MoreFeatures.jsx
@@ -8,17 +8,16 @@ const MoreFeatures = ({ content }) => {
     // Копируем и модифицируем content
     const modifiedContent = [...content];
     if (modifiedContent.length % 2 !== 0) {
-        modifiedContent.push(['', '']);
+        modifiedContent.push({ key: '', value: '' });
     }
 
     // Создаем result на основе modifiedContent
     const result = modifiedContent.reduce((acc, pair, index, arr) => {
         if (index % 2 === 0) {
-            const descriptions = arr[index + 1];
-            if (descriptions) {
-                pair.forEach((title, i) => {
-                    acc.push([title, descriptions[i]]);
-                });
+            const nextPair = arr[index + 1];
+            if (nextPair) {
+                acc.push({ key: pair.key, value: pair.value });
+                acc.push({ key: nextPair.key, value: nextPair.value });
             }
         }
         return acc;
@@ -30,8 +29,8 @@ const MoreFeatures = ({ content }) => {
                 <div className={cl.features}>
                     {result.map((feature, index) => (
                         <div key={`feature-${index}`} className={cl.featureItem}>
-                            <span>{feature[0]}</span>
-                            <span>{feature[1]}</span>
+                            <span>{feature.key}</span>
+                            <span>{feature.value}</span>
                         </div>
                     ))}
                 </div>
@@ -39,10 +38,10 @@ const MoreFeatures = ({ content }) => {
             {width <= 560 && (
                 <div className={cl.features}>
                     {modifiedContent.map((detail, index) => (
-                        Array.isArray(detail) && detail.length >= 2 && detail[0] !== '' && detail[1] !== '' && (
+                        detail.key !== '' && detail.value !== '' && (
                             <div key={`detail-${index}`} className={cl.detailsItemMobile}>
-                                <span>{detail[0]}</span>
-                                <span>{detail[1]}</span>
+                                <span>{detail.key}</span>
+                                <span>{detail.value}</span>
                             </div>
                         )
                     ))}
