8ec59acf2e49fc41442ef7032bfb52d3d280c0af
IvanGudyushkin <144389067+IvanGudyushkin@users.noreply.github.com>
2025-03-06 00:50:38 +0300

    delete garbage

--- src/Components/UI/Form/FormCall.jsx
+++ src/Components/UI/Form/FormCall.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import DOMPurify from 'dompurify';
 import axios from 'axios';
